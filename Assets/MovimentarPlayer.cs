f0e509fa7c7b87fa667a94f916ee87d57c69eacd
rodolfosouzamoraes <rodolfomsouza@gmail.com>
2025-02-13 08:39:48 -0300

    Pulo do personagem

--- Assets/MovimentarPlayer.cs
+++ Assets/MovimentarPlayer.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class MovimentarPlayer : MonoBehaviour
@@ -6,6 +7,7 @@ public class MovimentarPlayer : MonoBehaviour
     public LimitePlayer limiteDireita;
     public LimitePlayer limiteEsquerda;
     public LimitePlayer limitePe;
+    public Rigidbody2D rigidbody2d;
     public float velocidade; //Velocidade de movimentação do jogador
 
     public float forcaDoPuloY; //Força do pulo no eixo Y
@@ -17,6 +19,7 @@ public class MovimentarPlayer : MonoBehaviour
     void Update()
     {
         Movimentar();
+        Pular();
     }
 
     /// <summary>
@@ -51,7 +54,7 @@ public class MovimentarPlayer : MonoBehaviour
     private void Pular()
     {
         //Obter a entrada do usuário para efetuar o pulo
-        if (Input.GetKeyDown("Jump"))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             //Verificar se está apto a pular
             if(limitePe.estaNoLimite == true && estaPulando == false)
@@ -63,7 +66,62 @@ public class MovimentarPlayer : MonoBehaviour
                 puloDuplo = true;
 
                 //Ativar o tempo do pulo
+                AtivarTempoPulo();
             }
         }
+
+        EfetuarPulo();
+    }
+
+    /// <summary>
+    /// Método para fazer o player subir para simbolizar o pulo
+    /// </summary>
+    private void EfetuarPulo()
+    {
+        //Verificar se o player pode subir
+        if(estaPulando == true)
+        {
+            //Zerar as forças do rigidbody2D
+            rigidbody2d.linearVelocity = Vector3.zero;
+
+            //Alterar as propriedades do ridigbody2D para poder subir
+            rigidbody2d.gravityScale = 0;
+
+            //Direcionar o player para subir
+            Vector3 direcaoPulo = new Vector3(0, forcaDoPuloY, 0);
+
+            //Mover o player para direção de subida
+            transform.position += direcaoPulo * velocidade * Time.deltaTime;
+        }
+        else
+        {
+            //Reconfigurar as propriedades do rigibody2d para poder fazer o player descer
+            rigidbody2d.gravityScale = 4;
+        }
+    }
+
+    /// <summary>
+    /// Contar o tempo do pulo
+    /// </summary>
+    private IEnumerator TempoPulo()
+    {
+        //Permitir 0.3 de segundo para o player continuar subindo
+        yield return new WaitForSeconds(0.3f);//Espere 0.3 segundo para continuar o código
+
+        //Desabilitar a variavel que permite o pulo
+        estaPulando = false;
+    }
+
+    /// <summary>
+    /// Ativa a coroutine do tempo de pulo
+    /// </summary>
+    private void AtivarTempoPulo()
+    {
+        //Verificar se já existe uma coroutina contando o tempo
+        if(coroutinePulo != null)
+        {
+            StopCoroutine(coroutinePulo);
+        }
+        StartCoroutine(TempoPulo());
     }
 }
