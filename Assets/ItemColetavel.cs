dbcaf5f29bc462cb8f650e42846ecc9e818c53f3
rodolfosouzamoraes <rodolfomsouza@gmail.com>
2025-02-24 08:34:16 -0300

    Coletando Item

---
+++ Assets/ItemColetavel.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class ItemColetavel : MonoBehaviour
+{
+    public Animator animator;
+    private bool coletouItem;
+
+    private void OnTriggerEnter2D(Collider2D colisao)
+    {
+        //Verificar se o player colidiu com o item coletavel
+        if(colisao.gameObject.tag == "Player" && coletouItem == false)
+        {
+            //Informar que o item foi coletado
+            coletouItem = true;
+
+            //Ativar animação de coleta
+            animator.SetTrigger("Coletar");
+
+            //Incrementar o item no jogo
+
+        }
+    }
+
+    /// <summary>
+    /// Método para destruir o objeto após o fim da animação de coleta
+    /// </summary>
+    public void DestruirColetavel()
+    {
+        Destroy(gameObject);
+    }
+}
